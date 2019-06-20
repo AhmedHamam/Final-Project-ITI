9ef31a5f47c8d488d9041f3e78da5a764f85f92b
dinagouda <dinagouda1@gmail.com>
2019-06-20 15:44:27 +0200

    user login and register

---
+++ project/project/Controllers/CitzenController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project.Controllers
+{
+    public class CitzenController : Controller
+    {
+        // GET: Citzen
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        public ActionResult signup()
+        {
+            return View();
+        }
+
+    }
+}
