da7d5be58c1ed019f13fa9ed2834f40ce4c8c89f
Ahmed Hamam <ahmedhamam50@gmail.com>
2019-06-26 15:50:42 +0200

    addjobofficail

--- project/project/Models/OfficialJob.cs
+++ project/project/Models/OfficialJob.cs
@@ -14,16 +14,16 @@ namespace project.Models
             Officials = new HashSet<Official>();
         }
 
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+       
         public int id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="*")]
         [StringLength(500)]
         public string Job { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="*")]
         public string jobDescriptaion { get; set; }
-
+        public bool? isdeleted { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Official> Officials { get; set; }
     }
